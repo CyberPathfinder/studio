55a45092844061513740adf78f56316300d3cf7f
Александр Вальмус <aleks.Valmus2001@gmail.com>
2025-10-16 22:38:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4421779847-68b1c",
+  "appId": "1:55258673497:web:90a46c0c30e6cd27405c48",
+  "apiKey": "AIzaSyDfoMC7gFWN1s-4R4RPtNnSAyA2MzV4mnk",
+  "authDomain": "studio-4421779847-68b1c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "55258673497"
+};
