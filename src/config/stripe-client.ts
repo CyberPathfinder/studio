83dc716c0d912f9a090547cfdb7cf515b52b45c8
Александр Вальмус <aleks.Valmus2001@gmail.com>
2025-10-17 09:02:33 +0000

    Task: Настроить и подключить переменные окружения для Stripe, чтобы клие

--- src/config/stripe-client.ts
+++ src/config/stripe-client.ts
@@ -1,6 +1,7 @@
 const stripePublicKey = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
+
 if (!stripePublicKey) {
-  throw new Error('NEXT_PUBLIC_STRIPE_PUBLIC_KEY environment variable is not set.');
+    throw new Error('NEXT_PUBLIC_STRIPE_PUBLIC_KEY environment variable is not set.');
 }
 
 export { stripePublicKey };
